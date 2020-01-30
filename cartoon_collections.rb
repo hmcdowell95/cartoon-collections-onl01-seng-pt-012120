6c1c02be0dba61922664671f904c5e30bfbff85d
Harley McDowell <hmcdowell95@gmail.com>
2020-01-30 23:16:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  counter = 1 
+  dwarves.each do |name|
+    puts "#{counter}. #{name}"
+    counter += 1 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  na = []
+  planeteers.collect do |name|
+    word = name.capitalize
+    na << "#{word}!"
+  end
+  na
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? do |line|
+    line.length > 4
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
